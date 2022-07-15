2c22d11a745b4b9ae0348c2bfc2261f077b55cc9
unknown <vanphatct2002@gmail.com>
2022-07-15 17:43:00 +0700

    'upload'

---
+++ WinformAppConsole_/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace WinformAppConsole_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.imagePathToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tstxb_imgSource = new System.Windows.Forms.ToolStripTextBox();
+            this.rowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txbRow = new System.Windows.Forms.ToolStripTextBox();
+            this.columnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txbCol = new System.Windows.Forms.ToolStripTextBox();
+            this.mns_newGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlChessBoard = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.imagePathToolStripMenuItem,
+            this.tstxb_imgSource,
+            this.rowToolStripMenuItem,
+            this.txbRow,
+            this.columnToolStripMenuItem,
+            this.txbCol,
+            this.mns_newGame});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(881, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // imagePathToolStripMenuItem
+            // 
+            this.imagePathToolStripMenuItem.Enabled = false;
+            this.imagePathToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.imagePathToolStripMenuItem.Name = "imagePathToolStripMenuItem";
+            this.imagePathToolStripMenuItem.Size = new System.Drawing.Size(85, 23);
+            this.imagePathToolStripMenuItem.Text = "Image path:";
+            // 
+            // tstxb_imgSource
+            // 
+            this.tstxb_imgSource.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.tstxb_imgSource.Name = "tstxb_imgSource";
+            this.tstxb_imgSource.Size = new System.Drawing.Size(300, 23);
+            this.tstxb_imgSource.Text = "D:\\Ảnh\\icon-pokemon-go.jpg";
+            this.tstxb_imgSource.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tstxb_imgSource_KeyDown);
+            // 
+            // rowToolStripMenuItem
+            // 
+            this.rowToolStripMenuItem.Enabled = false;
+            this.rowToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.rowToolStripMenuItem.Name = "rowToolStripMenuItem";
+            this.rowToolStripMenuItem.Size = new System.Drawing.Size(44, 23);
+            this.rowToolStripMenuItem.Text = "Row";
+            // 
+            // txbRow
+            // 
+            this.txbRow.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txbRow.Name = "txbRow";
+            this.txbRow.Size = new System.Drawing.Size(50, 23);
+            this.txbRow.Text = "3";
+            this.txbRow.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tstxb_imgSource_KeyDown);
+            // 
+            // columnToolStripMenuItem
+            // 
+            this.columnToolStripMenuItem.Enabled = false;
+            this.columnToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.columnToolStripMenuItem.Name = "columnToolStripMenuItem";
+            this.columnToolStripMenuItem.Size = new System.Drawing.Size(61, 23);
+            this.columnToolStripMenuItem.Text = "Column";
+            // 
+            // txbCol
+            // 
+            this.txbCol.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txbCol.Name = "txbCol";
+            this.txbCol.Size = new System.Drawing.Size(50, 23);
+            this.txbCol.Text = "3";
+            this.txbCol.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tstxb_imgSource_KeyDown);
+            // 
+            // mns_newGame
+            // 
+            this.mns_newGame.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.mns_newGame.Name = "mns_newGame";
+            this.mns_newGame.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.mns_newGame.Size = new System.Drawing.Size(79, 23);
+            this.mns_newGame.Text = "New game";
+            this.mns_newGame.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // pnlChessBoard
+            // 
+            this.pnlChessBoard.Location = new System.Drawing.Point(12, 27);
+            this.pnlChessBoard.Name = "pnlChessBoard";
+            this.pnlChessBoard.Size = new System.Drawing.Size(857, 372);
+            this.pnlChessBoard.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 411);
+            this.Controls.Add(this.pnlChessBoard);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mns_newGame;
+        private System.Windows.Forms.Panel pnlChessBoard;
+        private System.Windows.Forms.ToolStripTextBox tstxb_imgSource;
+        private System.Windows.Forms.ToolStripMenuItem imagePathToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox txbRow;
+        private System.Windows.Forms.ToolStripMenuItem columnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox txbCol;
+    }
+}
+
---
+++ WinformAppConsole_/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformAppConsole_
+{
+    public partial class Form1 : Form
+    {
+        #region Properties
+        PictureBox tempPtb = null;
+        List<Button> btnBoard;
+        Point imgStartPoint = new Point(-1, -1);
+        int index = -1;
+        int countTrueIndex = 0;
+        int row = 10;
+        int col = 10;
+        const int maxWidth = 500;
+        const int maxHeight = 500;
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnBoard = new List<Button>();
+
+            InitGame();
+        }
+
+        #region Events  
+        private void Ptb_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (tempPtb != null)
+            {
+                Point poitntoclient = PointToClient(MousePosition);
+
+                tempPtb.Location = new Point(poitntoclient.X - pnlChessBoard.Location.X, poitntoclient.Y - pnlChessBoard.Location.Y);
+
+                foreach (var btn in btnBoard)
+                {
+                    if (InRange(tempPtb.Location.X, tempPtb.Location.Y, btn.Location, btn.Width, btn.Height))
+                    {
+                        index = (int)btn.Tag;
+                        return;
+                    }
+                }
+
+                index = -1;
+            }
+        }
+
+        private void Ptb_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (index != -1)
+            {
+                tempPtb.Location = btnBoard[index].Location;
+
+                if (index == (int)tempPtb.Tag)
+                {
+                    countTrueIndex++;
+                    if (countTrueIndex == row * col)
+                    {
+                        MessageBox.Show("Trò chơi kết thúc!");
+
+                        SetAllControl(false);
+                    }
+                }
+            }
+            else
+            {
+                // trả về vị trí ban đầu
+                tempPtb.Location = imgStartPoint;
+            }
+
+            // trạng thái huỷ
+            tempPtb = null;
+            index = -1;
+        }
+
+        private void Ptb_MouseDown(object sender, MouseEventArgs e)
+        {
+            tempPtb = sender as PictureBox;
+            imgStartPoint = (sender as PictureBox).Location;
+            index = -1;
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetAllControl(true);
+            InitGame();
+        }
+
+        private void tstxb_imgSource_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                mns_newGame.PerformClick();
+            }
+        }
+        #endregion
+
+
+        #region Methods
+        void InitGame()
+        {
+            try
+            {
+                row = int.Parse(txbRow.Text);
+                col = int.Parse(txbCol.Text);
+            }
+            catch { }
+
+            if (row * col > 1500)
+            {
+                MessageBox.Show("Không thể cắt ảnh vượt qua mức 1500!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            countTrueIndex = 0;
+            pnlChessBoard.Controls.Clear();
+
+            try
+            {
+                Image img = Image.FromFile(tstxb_imgSource.Text); 
+
+                if (img.Width > maxWidth || img.Height > maxHeight)
+                {
+                    MessageBox.Show("Kích thước hình ảnh quá lớn! Vui lòng chọn ảnh có kích thước tối đa là 500x500", "Lỗi tải hình ảnh", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                
+                CreateImageBoard(img);
+                CreateButtonBoard(img.Width, img.Height);
+            }
+            catch 
+            {
+                MessageBox.Show("Hình ảnh không tồn tại! Yêu cầu xem lại đường dẫn!", "Lỗi tải hình ảnh", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }            
+        }
+
+        void SetAllControl(bool enable)
+        {
+            foreach (var item in pnlChessBoard.Controls)
+            {
+                try
+                {
+                    if ((item as Button) != null)
+                    {
+                        (item as Button).Enabled = enable;
+                    }
+                    else if ((item as PictureBox) != null)
+                    {
+                        (item as PictureBox).Enabled = enable;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        bool InRange(int x, int y, Point startPoint, int width, int height)
+        {
+            return startPoint.X <= x && x <= startPoint.X + width && startPoint.Y <= y && y <= startPoint.Y + height;
+        }
+
+        Bitmap DropImage(Image img, int X, int Y, int width, int height)
+        {
+            Bitmap bmp = new Bitmap(width, height);
+
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawImage(img, new Rectangle(0, 0, width, height), new Rectangle(X, Y, width, height), GraphicsUnit.Pixel);
+            g.Dispose();
+
+            return bmp;
+        }
+
+        List<Thongtin> SplitImage(Image img, int row, int col)
+        {
+            List<Thongtin> ptbList = new List<Thongtin>();
+
+            int cutHeight = (int)(img.Height * 1.0 / row);
+            int cutWidth = (int)(img.Width * 1.0 / col);
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Point p = new Point(j * cutWidth, i * cutHeight);
+
+                    PictureBox ptb = new PictureBox()
+                    {
+                        Image = DropImage(img, p.X, p.Y, cutWidth, cutHeight),
+                        Width = cutWidth,
+                        Height = cutHeight,
+                        Tag = i * col + j,
+                        //Location = new Point(p.X + j, p.Y + i)
+                    };
+
+                    ptb.MouseDown += Ptb_MouseDown;
+                    ptb.MouseUp += Ptb_MouseUp;
+                    ptb.MouseMove += Ptb_MouseMove;
+
+                    ptbList.Add(new Thongtin(ptb, i * col + j));
+                }
+            }
+
+            return ptbList;
+        }
+
+        void CreateImageBoard(Image img)
+        {
+            int cutHeight = (int)(img.Height * 1.0 / row);
+            int cutWidth = (int)(img.Width * 1.0 / col);
+            List<Thongtin> temp = SplitImage(img, row, col);
+            List<Thongtin> map = new List<Thongtin>();
+
+            Random rand = new Random();
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    int index = rand.Next(temp.Count);
+                    Thongtin t = temp[index];
+                    t.Ptb.Location = new Point(j * (cutWidth + 1), i * (cutHeight + 1));
+                    map.Add(t);
+                    temp.RemoveAt(index);
+                }
+            }
+
+            foreach (var item in map)
+            {
+                pnlChessBoard.Controls.Add(item.Ptb);
+            }
+        }
+
+        void CreateButtonBoard(int imgWidth, int imgHeight)
+        {
+            btnBoard.Clear();
+
+            int cutHeight = (int)(imgHeight * 1.0 / row);
+            int cutWidth = (int)(imgWidth * 1.0 / col);
+
+            Point startPoint = new Point(pnlChessBoard.Width - imgWidth, 0);
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Button btn = new Button()
+                    {
+                        Width = cutWidth,
+                        Height = cutHeight,
+                        Tag = i * col + j,
+                        Location = new Point(startPoint.X + j * cutWidth, startPoint.Y + i * cutHeight)
+                    };
+
+                    btnBoard.Add(btn);
+                    pnlChessBoard.Controls.Add(btn);
+                }
+            }
+
+        }
+        #endregion
+
+    }
+
+    public class Thongtin
+    {
+        public PictureBox Ptb { get; set; }
+        public int Index { get; set; }
+
+        public Thongtin(PictureBox ptb, int index)
+        {
+            this.Ptb = ptb;
+            this.Index = index;
+        }
+    }
+}
